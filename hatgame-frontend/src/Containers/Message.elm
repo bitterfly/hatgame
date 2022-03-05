89036c02643633d12264414c3fceec37400c6c87
Diana Geneva <dageneva@qtrp.org>
2022-03-05 10:59:32 +0200

    [frontend] Separate received and sent messages
    Also rename some of the messages to better reflect their action.

--- hatgame-frontend/src/Containers/Message.elm
+++ hatgame-frontend/src/Containers/Message.elm
@@ -1,41 +1,75 @@
-module Containers.Message exposing (Message(..), decode, decodeMsgString)
+module Containers.Message exposing (MessageReceived(..), MessageSend(..), decode, decodeMsgString, encodeMsgSend)
 
 import Containers.Game
 import Json.Decode exposing (Decoder)
+import Json.Encode
 
 
-type Message
+type MessageReceived
     = Game Containers.Game.Game
-    | Word String
+    | ReceiveAddWord String
     | Team Int
     | Story String
     | Tick Int
-    | Started Int
+    | GuessPhaseStart Int
     | Ended (List Containers.Game.Team)
     | Error String
+    | ReadyToStart
+    | WordPhaseStart
 
 
-decode : Decoder Message
+type MessageSend
+    = ReadyStoryteller
+    | RequestToStart
+    | Guess String
+    | SendAddWord String
+
+
+encodeMsgSend : MessageSend -> String
+encodeMsgSend msg =
+    Json.Encode.encode 0 <|
+        case msg of
+            ReadyStoryteller ->
+                Json.Encode.object [ ( "type", Json.Encode.string "ready_storyteller" ) ]
+
+            RequestToStart ->
+                Json.Encode.object
+                    [ ( "type", Json.Encode.string "request_to_start" ) ]
+
+            Guess word ->
+                Json.Encode.object
+                    [ ( "type", Json.Encode.string "guess" )
+                    , ( "msg", Json.Encode.string word )
+                    ]
+
+            SendAddWord word ->
+                Json.Encode.object
+                    [ ( "type", Json.Encode.string "add_word" )
+                    , ( "msg", Json.Encode.string word )
+                    ]
+
+
+decode : Decoder MessageReceived
 decode =
     Json.Decode.andThen decodeMsgString (Json.Decode.field "Type" Json.Decode.string)
 
 
-decodeMsgString : String -> Decoder Message
+decodeMsgString : String -> Decoder MessageReceived
 decodeMsgString str =
     case str of
         "game" ->
             Json.Decode.map Game
                 (Json.Decode.field "Msg" Containers.Game.decode)
 
-        "word" ->
-            Json.Decode.map Word
+        "add_word" ->
+            Json.Decode.map ReceiveAddWord
                 (Json.Decode.field "Msg" Json.Decode.string)
 
         "team" ->
             Json.Decode.map Team <| Json.Decode.field "Msg" Json.Decode.int
 
-        "start" ->
-            Json.Decode.map Started <| Json.Decode.field "Msg" Json.Decode.int
+        "guess_phase_start" ->
+            Json.Decode.map GuessPhaseStart <| Json.Decode.field "Msg" Json.Decode.int
 
         "end" ->
             Json.Decode.map Ended <| Json.Decode.field "Msg" <| Json.Decode.list Containers.Game.decodeTeam
@@ -48,6 +82,12 @@ decodeMsgString str =
 
         "error" ->
             Json.Decode.map Error <| Json.Decode.field "Msg" Json.Decode.string
+
+        "ready_to_start" ->
+            Json.Decode.succeed ReadyToStart
+
+        "word_phase_start" ->
+            Json.Decode.succeed WordPhaseStart
 
         x ->
             Json.Decode.fail <| "message not recognised " ++ x
